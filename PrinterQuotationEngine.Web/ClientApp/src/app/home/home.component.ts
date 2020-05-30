4d167ef6fa9e6856f4936c75ac3f1f5cd56141f6
Shawn Jackson <shawn@resgrid.com>
2020-05-30 08:54:10 -0700

    Working on the wizard

--- PrinterQuotationEngine.Web/ClientApp/src/app/home/home.component.ts
+++ PrinterQuotationEngine.Web/ClientApp/src/app/home/home.component.ts
@@ -4,18 +4,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit  {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-  @ViewChild("fileDropRef", { static: false }) fileDropEl: ElementRef;
+  @ViewChild('fileDropRef', { static: false }) fileDropEl: ElementRef;
   files: any[] = [];
 
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+      firstNameCtrl: ['', Validators.required],
+      lastNameCtrl: ['', Validators.required],
+      emailCtrl: ['', Validators.required],
+      companyNameCtrl: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required]
